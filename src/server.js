7159ce050bb779f3ec3b20faec069aeb355862a3
dokilove <38000425+dokilove@users.noreply.github.com>
2022-04-20 16:19:31 +0900

    #3.3 Responses (11:23)

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("I still love you");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login Here");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
 
