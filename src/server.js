1724de4fa7ee252319fc50368cd2cf4cc2306729
dokilove <38000425+dokilove@users.noreply.github.com>
2022-04-21 15:25:15 +0900

    #3.5 Middlewares part One (09:17)

--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to : ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("I still love you");
+  return res.send("I love middlewares");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("Login Here");
-};
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
