c3214f8cb6b229fc823aad74bee943c818d25a34
dokilove <38000425+dokilove@users.noreply.github.com>
2022-04-22 15:50:46 +0900

    #3.10 Middleware Recap (12:53)

--- src/server.js
+++ src/server.js
@@ -1,20 +1,32 @@
 import express from "express";
+import { route } from "express/lib/application";
 
 const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
+const routerLogger = (req, res, next) => {
+  console.log("PATH", req.path);
   next();
 };
 
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
+const methodLogger = (req, res, next) => {
+  console.log("METHOD", req.method);
+  next();
 };
 
-app.use(logger);
-app.get("/", handleHome);
+const home = (req, res) => {
+  console.log("I will respond");
+  return res.send("<h1>Hello</h1>");
+};
+
+const login = (req, res) => {
+  return res.send("<h2>login</h2>");
+};
+
+app.use(methodLogger, routerLogger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
