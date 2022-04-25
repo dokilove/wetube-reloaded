0ca6f47ced97cb02cf6a90a8f04a4ca0ad4e94bd
dokilove <38000425+dokilove@users.noreply.github.com>
2022-04-25 15:21:51 +0900

    #4.1 Making Our Routers (11:05)

--- src/server.js
+++ src/server.js
@@ -1,23 +1,34 @@
 import express from "express";
+import { Router } from "express";
 import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  console.log("I will respond");
-  return res.send("<h1>Hello</h1>");
-};
-
-const login = (req, res) => {
-  return res.send("<h2>login</h2>");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit user");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}`);
