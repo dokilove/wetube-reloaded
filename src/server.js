7e6c99c564e479836e43876fc856ac0da9213841
dokilove <38000425+dokilove@users.noreply.github.com>
2022-05-03 22:22:45 +0900

    #6.4 Recap (9:00)

--- src/server.js
+++ src/server.js
@@ -12,10 +12,11 @@ const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
+// pug 초기 세팅 폴더가 package.json있는데를 기준으로 하기 때문에 변경
 app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
-// form을 자바스크립트 형식으로 먼저 변환시켜야하기 때문에
+// form의 value를 자바스크립트 형식으로 먼저 변환시켜야하기 때문에
 // 우리가 쓰는 라우터보다 먼저 적용해야 한다
 app.use(express.urlencoded({ extended: true }));
 
