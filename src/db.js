b12a015a1ec4ce9e2aeea75a9bde36c6078261b6
dokilove <38000425+dokilove@users.noreply.github.com>
2022-05-06 16:12:53 +0900

    #6.10 Video Model (09:06)

--- src/db.js
+++ src/db.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 mongoose.connect("mongodb://127.0.0.1:27017/wetube");
 // warning 안떠서 그냥 진행
+// 문제 생기면 #6.8 9분경으로 가서 보자
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("Connected to DB");
-const handleError = () => (error) => console.log("DB Error", error);
+const handleOpen = () => console.log("✓ Connected to DB");
+const handleError = () => (error) => console.log("✋ DB Error", error);
 
 db.on("error", handleError);
 db.once("open", handleOpen);
