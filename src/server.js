846ae18046d5606e27971afe9d68f5b5574c80a1
dokilove <38000425+dokilove@users.noreply.github.com>
2022-05-10 23:09:26 +0900

    #6.11 Our First Query (09:58)

--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 // console.log(process.cwd());
 
@@ -26,7 +22,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
