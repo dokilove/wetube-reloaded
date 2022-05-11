30e1fb28630826689edcb85ff36fe57c4b3ce427
dokilove <38000425+dokilove@users.noreply.github.com>
2022-05-11 15:11:59 +0900

    #6.16 Creating a Video part Two (12:21)

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -4,6 +4,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
+  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
@@ -24,12 +25,12 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title,
     description,
-    createdAt: Date.now(),
+    createdAt: "vreate at",
     hashtags: hashtags
       .split(",")
       .map((word) =>
@@ -40,6 +41,5 @@ export const postUpload = (req, res) => {
       rating: 0,
     },
   });
-  console.log(video);
   return res.redirect("/");
 };
