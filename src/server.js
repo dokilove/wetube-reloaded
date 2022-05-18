86384669eeffd762771e7d839499f4752600acc6
dokilove <38000425+dokilove@users.noreply.github.com>
2022-05-18 12:44:05 +0900

    #7.0 Create Account part One (11:34)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
@@ -18,7 +18,7 @@ app.use(logger);
 // 우리가 쓰는 라우터보다 먼저 적용해야 한다
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
