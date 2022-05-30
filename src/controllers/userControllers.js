6aa03b38130ac04b4faf06e21782cf288b866073
dokilove <38000425+dokilove@users.noreply.github.com>
2022-05-30 14:50:32 +0900

    #7.21 Github Login part Six (13:54)

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -108,13 +108,30 @@ export const finishGithubLogin = async (req, res) => {
       })
     ).json();
     // console.log(emailData);
-    const email = emailData.find(
+    const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
-    if (!email) {
+    if (!emailObj) {
       return res.redirect("/login");
     }
-    console.log(email);
+    const existingUser = await User.findOne({ email: emailObj.email });
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+      return res.redirect("/");
+    } else {
+      const user = await User.create({
+        name: userData.name ? userData.name : userData.login,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect("/");
+    }
   } else {
     return res.redirect("/login");
   }
