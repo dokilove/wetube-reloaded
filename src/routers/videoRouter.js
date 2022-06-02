d5104e50370daa00871063d5031b42c4dd07c437
dokilove <38000425+dokilove@users.noreply.github.com>
2022-06-02 14:27:43 +0900

    #8.2 Edit Profile POST part One (12:04)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { all } from "express/lib/application";
 import {
   watch,
   getEdit,
@@ -7,12 +8,24 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/videoControllers";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.route("/:id([0-9a-f]{24})").get(watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 
 export default videoRouter;
