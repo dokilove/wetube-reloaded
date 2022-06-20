a18ce49a3ecbfa4157587a9c01d223316c50abe9
dokilove <38000425+dokilove@users.noreply.github.com>
2022-06-20 09:47:37 +0900

    #8.7 File Uploads part Two (10:51)

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -246,7 +246,7 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
     file,
@@ -279,6 +279,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
