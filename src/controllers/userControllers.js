5e458a43d1e38aba5071ee7f30a54bcf1ea89323
dokilove <38000425+dokilove@users.noreply.github.com>
2022-06-23 07:21:18 +0900

    #8.13 User's Videos (13:05)

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -328,17 +328,15 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
+  console.log(user);
 
   if (!user) {
     return res.status(404).render("404");
   }
-  const videos = await Video.find({ owner: user._id });
 
   return res.render("users/profile", {
     pageTitle: user.name,
     user,
-    videos,
   });
 };
