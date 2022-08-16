0629f7996e184bc055979f9bd8d728cac8768f53
dokilove <38000425+dokilove@users.noreply.github.com>
2022-08-16 13:54:31 +0900

    #14.1 Transcode Video (12:02)

--- src/server.js
+++ src/server.js
@@ -17,6 +17,13 @@ app.set("view engine", "pug");
 // pug 초기 세팅 폴더가 package.json있는데를 기준으로 하기 때문에 변경
 app.set("views", process.cwd() + "/src/views");
 
+//ffmpeg 관련 에러 해결
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
+
 app.use(logger);
 // form의 value를 자바스크립트 형식으로 먼저 변환시켜야하기 때문에
 // 우리가 쓰는 라우터보다 먼저 적용해야 한다
