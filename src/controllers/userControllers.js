ad446ef4430616ac514287e9424480de443aa6c3
dokilove <38000425+dokilove@users.noreply.github.com>
2022-08-30 14:12:58 +0900

    #15.0 Installation (06:39)

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -238,6 +238,7 @@ export const finishKakaoLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   req.session.destroy();
+  req.flash("info", "Bye Bye");
   return res.redirect("/");
 };
 
@@ -292,6 +293,7 @@ export const postEdit = async (req, res) => {
 
 export const getChangePassword = (req, res) => {
   if (req.session.user.socialOnly === true) {
+    req.flash("error", "Can't change password.");
     return res.redirect("/");
   }
   return res.render("users/change-password", { pageTitle: "Change Password" });
@@ -323,6 +325,7 @@ export const postChangePassword = async (req, res) => {
   user.password = newPassword;
   await user.save();
 
+  req.flash("info", "Password updated");
   return res.redirect("/users/logout");
 };
 
