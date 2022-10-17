2e82f3dd6beee4b33142fddb947c8a119a1650c4
dokilove <38000425+dokilove@users.noreply.github.com>
2022-10-17 20:13:42 +0900

    cross origin modified2

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "");
+  res.header("Cross-Origin-Embedder-Policy", "credentialless");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
